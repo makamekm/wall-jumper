9f9ed1177cfcd34f1b00e70c635c10269f50c221
makame <makame@mail.ru>
2020-01-03 01:18:24 +0300

    add simple level obj

---
+++ Assets/Levels/SimpleLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class SimpleLevel : GameLevel
+{
+    public override GameSystem[] Systems { get; } = {
+        // new System()
+    };
+
+    void Start()
+    {
+        GameManager.Level = this;
+    }
+}
